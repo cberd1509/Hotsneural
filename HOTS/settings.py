89eb717d6f8e65ef6509e7cc3a07a49a97031b87
CARLOS BERDUGO <CARLOS.BERDUGO1@correo.uis.edu.co>
2018-08-11 21:05:45 -0500

    Agregado procfile

--- HOTS/settings.py
+++ HOTS/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+print(os.path.join(BASE_DIR, 'static'))
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    '/static/'
+    os.path.join(BASE_DIR, 'static'),
 )
