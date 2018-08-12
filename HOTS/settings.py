36e6b3ec70f34a698885138e3eef88beae8646af
CARLOS BERDUGO <CARLOS.BERDUGO1@correo.uis.edu.co>
2018-08-11 19:06:12 -0500

    Agregado procfile

--- HOTS/settings.py
+++ HOTS/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = '/staticfiles'
+STATIC_ROOT = '/files/'
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join('/static/'),
 )
