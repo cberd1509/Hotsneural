3e4146e2f60e031335879550098792b786632735
CARLOS BERDUGO <CARLOS.BERDUGO1@correo.uis.edu.co>
2018-08-12 20:14:24 -0500

    Subida de Evaluación Económica

--- HotsNeural/views.py
+++ HotsNeural/views.py
@@ -32,10 +32,19 @@ def forecast(request):
 	if request.method == "POST":
 		Formdata = request.POST
 
+		eorMethod="Inyección Contínua de Vapor"
+
+		eorDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tristique rutrum dui, a facilisis ante sodales vitae. Sed venenatis metus purus, id fringilla urna interdum rutrum. In et est nec leo blandit ornare. Nullam vehicula massa ex, nec placerat nibh pharetra at. Suspendisse vel ipsum velit. Fusce bibendum nisi vitae ligula cursus, et placerat est hendrerit. Sed euismod finibus elit non tristique. Aliquam rutrum velit sit amet dignissim egestas. Aliquam ut quam commodo, varius felis at, condimentum purus. Vivamus gravida pellentesque turpis, vel tincidunt metus dapibus sed. Mauris leo eros, dapibus scelerisque sem et, porta malesuada lorem. Etiam id lorem a turpis aliquam iaculis. Vivamus vel bibendum dui, vitae cursus urna."
+
+		VPN = 250000000
+		TIR = 80
+		PBT = 3
+		ROI = 50
+
 		row = [5000,4200,3500,2400,1900]
 		jsonArray = json.dumps(row)
 
-		return render(request, 'Results.html',dict(DailyProd=jsonArray));
+		return render(request, 'Results.html',dict(DailyProd=jsonArray, eorMethod=eorMethod, eorDescription=eorDescription, VPN=VPN, TIR=TIR, PBT=PBT, ROI=ROI));
 
 
 
